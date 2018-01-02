b3a1a680d6a7f4f5194d065d676269df364a894a
samguitarboy <sam65ocean@gmail.com>
2018-01-02 15:49:46 +0800

    revise2

---
+++ APOLO/src/apolo_setup/GamepageController.java
@@ -0,0 +1,115 @@
+package apolo_setup;
+
+import javafx.fxml.FXML;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.Event;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.shape.Circle;
+
+public class GamepageController {
+
+    @FXML
+    private Button main1;
+    @FXML
+    private Circle node1, node2, node3;
+    @FXML
+    private StackPane scene;
+
+    public void initialize() {
+        // TODO
+
+    }
+
+    public void postInit() {
+        String Path = System.getProperty("user.dir") + "/src/main/resources/hit.mp3";
+        Media hit = new Media(new File(Path).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(hit);
+
+        Timer timer = new Timer();
+        node1.setVisible(true);
+        node2.setVisible(true);
+        node3.setVisible(true);
+        TimerTask nodemove = new TimerTask() {
+            int count = -420;
+            int temp;
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub   
+                node1.setTranslateY(count);
+                if (count < 350) {
+                    temp = count - 50;
+                }
+                node2.setTranslateY(temp);
+                node3.setTranslateY(count + 30);
+                count += 1;
+            }
+        };
+        timer.schedule(nodemove, 0, 2);//execute speed 
+        scene.setOnKeyPressed((Event event) -> {
+            mediaPlayer.play();
+            if (null != event.toString().substring(181)) {
+                switch (event.toString().substring(181)) {
+                    case " LEFT]":
+                        
+                        node1.setVisible(false);
+                         {
+                            try {
+                                this.writefile(String.valueOf(node1.getTranslateY()));
+                            } catch (IOException ex) {
+                                Logger.getLogger(GamepageController.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                        }
+                        break;
+                    case " RIGHT]":
+                        node3.setVisible(false);
+                         {
+                            try {
+                                this.writefile(String.valueOf(node2.getTranslateY()));
+                            } catch (IOException ex) {
+                                Logger.getLogger(GamepageController.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                        }
+                        break;
+                    case " UP]":
+                        node2.setVisible(false);
+                        //System.out.println(event.toString().substring(181));
+                         {
+                            try {
+                                this.writefile(String.valueOf(node3.getTranslateY()));
+                            } catch (IOException ex) {
+                                Logger.getLogger(GamepageController.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            //System.out.println(event.toString().substring(181));
+        });
+
+    }
+
+    public void dispose() {
+
+    }
+
+    public void writefile(String pos) throws IOException {
+        String path = System.getProperty("user.dir") + "/src/main/resources/test.txt";
+        FileWriter fw = new FileWriter(path, true);
+        fw.write(pos + " ");
+        System.out.println("ok");
+        fw.close();
+    }
+}
--- APOLO/src/apolo_setup/MainpageController.java
+++ APOLO/src/apolo_setup/MainpageController.java
@@ -4,10 +4,18 @@ import com.jfoenix.controls.JFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class MainpageController implements Initializable {
 
@@ -15,6 +23,26 @@ public class MainpageController implements Initializable {
     private JFXButton stop;
     @FXML
     private Label label;
+
+    @FXML
+    private void about() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Apolo");
+        alert.setHeaderText("About Apolo");
+        alert.setGraphic(new ImageView(new Image("images/about.png")));
+        alert.setContentText("Designed by Sam.Chen & Eddie.Lu");
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void gotoPlayingPage(MouseEvent event) throws Exception {
+        Parent main_page_parent = FXMLLoader.load(getClass().getResource("gamepage.fxml"));
+        Scene main_page_scene = new Scene(main_page_parent);
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.setScene(main_page_scene);
+        app_stage.show();
+
+    }
 
     @FXML
     private void playmp3(MouseEvent event) throws Exception {
@@ -33,6 +61,7 @@ public class MainpageController implements Initializable {
     }
 
     private static class Playing implements Runnable {
+
         @Override
         public void run() {
             try {
