d5e3c6477fb5b0e397e49a54610b7ff11e32a052
s1031416 <s091139@gmail.com>
2018-01-02 19:25:40 +0800

    score

--- APOLO/src/apolo_setup/GamepageController.java
+++ APOLO/src/apolo_setup/GamepageController.java
@@ -2,7 +2,6 @@ package apolo_setup;
 
 import javafx.fxml.FXML;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Timer;
@@ -17,8 +16,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
@@ -30,54 +27,61 @@ public class GamepageController {
     private Circle node1, node2, node3;
     @FXML
     private StackPane scene;
+    
+    int score=0;
 
     public void initialize() {
-        // TODO
-
-    }
-    @FXML
-    private void gotoMain(MouseEvent event) throws Exception {
-        Parent main_page_parent = FXMLLoader.load(getClass().getResource("mainpage.fxml"));
-        Scene main_page_scene = new Scene(main_page_parent);
-        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        app_stage.setScene(main_page_scene);
-        app_stage.show();
-
-    }
-    
-    public void postInit() {
-        String Path = System.getProperty("user.dir") + "/src/main/resources/hit.mp3";
-        Media hit = new Media(new File(Path).toURI().toString());
-        MediaPlayer mediaPlayer = new MediaPlayer(hit);
-
+        Circle circle = new Circle();
+        circle.setVisible(true);
+        circle.setId("newnode");
+        circle.setTranslateY(500);
+        circle.setTranslateX(200);
+        
+        try {
+            this.fileclear();
+        } catch (IOException ex) {
+            Logger.getLogger(GamepageController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         Timer timer = new Timer();
-        node1.setVisible(true);
-        node2.setVisible(true);
-        node3.setVisible(true);
         TimerTask nodemove = new TimerTask() {
-            int count = -420;
-            int temp;
+            int count = 0;
+            int temp = 0;
+            int temp2 = 0;
 
             @Override
-            public void run() {
+            public void run() { // 650 標準點 
                 // TODO Auto-generated method stub   
                 node1.setTranslateY(count);
-                if (count < 350) {
-                    temp = count - 50;
-                }
                 node2.setTranslateY(temp);
-                node3.setTranslateY(count + 30);
+                node3.setTranslateY(temp2);
                 count += 1;
+                temp += 1;
+                temp2 += 1;
+                if (count > 800) {
+                    count = 0;
+                    node1.setVisible(true);
+                }
+                if (temp > 800) {
+                    temp = -420;
+                    node2.setVisible(true);
+                }
+                if (temp2 > 800) {
+                    temp2 = 0;
+                    node3.setVisible(true);
+                }
             }
         };
         timer.schedule(nodemove, 0, 2);//execute speed 
         scene.setOnKeyPressed((Event event) -> {
-            mediaPlayer.play();
             if (null != event.toString().substring(181)) {
                 switch (event.toString().substring(181)) {
                     case " LEFT]":
-                        
                         node1.setVisible(false);
+                        if (node1.getTranslateY()<670 && node1.getTranslateY()>630)
+                            score+=100;
+                        else if(node1.getTranslateY()>670 && node1.getTranslateY()<690 ||node1.getTranslateY()<630 && node1.getTranslateY()>610)
+                            score+=50;
+                            
                          {
                             try {
                                 this.writefile(String.valueOf(node1.getTranslateY()));
@@ -88,6 +92,11 @@ public class GamepageController {
                         break;
                     case " RIGHT]":
                         node3.setVisible(false);
+                        if (node1.getTranslateY()<670 && node1.getTranslateY()>630)
+                            score+=100;
+                        else if(node1.getTranslateY()>670 && node1.getTranslateY()<690 ||node1.getTranslateY()<630 && node1.getTranslateY()>610)
+                            score+=50;
+                            
                          {
                             try {
                                 this.writefile(String.valueOf(node2.getTranslateY()));
@@ -98,6 +107,11 @@ public class GamepageController {
                         break;
                     case " UP]":
                         node2.setVisible(false);
+                        if (node1.getTranslateY()<670 && node1.getTranslateY()>630)
+                            score+=100;
+                        else if(node1.getTranslateY()>670 && node1.getTranslateY()<690 ||node1.getTranslateY()<630 && node1.getTranslateY()>610)
+                            score+=50;
+                            
                         //System.out.println(event.toString().substring(181));
                          {
                             try {
@@ -111,8 +125,20 @@ public class GamepageController {
                         break;
                 }
             }
-            //System.out.println(event.toString().substring(181));
         });
+    }
+
+    @FXML
+    private void gotoMain(MouseEvent event) throws Exception {
+        Parent main_page_parent = FXMLLoader.load(getClass().getResource("mainpage.fxml"));
+        Scene main_page_scene = new Scene(main_page_parent);
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.setScene(main_page_scene);
+        app_stage.show();
+
+    }
+
+    public void postInit() {
 
     }
 
@@ -120,11 +146,18 @@ public class GamepageController {
 
     }
 
+    public void fileclear() throws IOException {
+        String path = System.getProperty("user.dir") + "/src/music/test.txt";
+        try (FileWriter fw = new FileWriter(path)) {
+            fw.write("");
+        }
+    }
+
     public void writefile(String pos) throws IOException {
-        String path = System.getProperty("user.dir") + "/src/main/resources/test.txt";
-        FileWriter fw = new FileWriter(path, true);
-        fw.write(pos + " ");
-        System.out.println("ok");
-        fw.close();
+        String path = System.getProperty("user.dir") + "/src/music/test.txt";
+        try (FileWriter fw = new FileWriter(path, true)) {
+            fw.write(pos + " ");
+            System.out.println(score);
+        }
     }
 }
